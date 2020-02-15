2ce72bc1cb48e259e088a166baf43feb00c3bb82
Ovidiu Pruteanu <ovidiu@wmtek.com>
2020-02-15 21:53:54 +0200

    Door Lock Color Indicator smart app

---
+++ smartapps/ovidiupruteanu/door-lock-color-indicator.src/door-lock-color-indicator.groovy
@@ -0,0 +1,42 @@
+/**
+ *  Door Lock Color Indicator
+ */
+definition(
+        name: "Door Lock Color Indicator",
+        namespace: "ovidiupruteanu",
+        author: "Ovidiu Pruteanu",
+        description: "Door Lock Color Indicator",
+        category: "Convenience",
+        iconUrl: "https://github.com/chancsc/icon/raw/master/standard-tile%401x.png",
+        iconX2Url: "https://github.com/chancsc/icon/raw/master/standard-tile@2x.png",
+        iconX3Url: "https://github.com/chancsc/icon/raw/master/standard-tile@3x.png"
+)
+
+preferences {
+    section() {
+        input "doorLock", "capability.lock", title: "Door Lock"
+        input "indicator", "capability.execute", title: "Color Indicator"
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    subscribe(doorLock, "lock.locked", doorLocked)
+    subscribe(doorLock, "lock.unlocked", doorUnlocked)
+}
+
+def doorLocked(event) {
+    indicator.execute("colorWhenOnOrOff", new groovy.json.JsonBuilder([color: "red"]).toString())
+}
+
+def doorUnlocked(event) {
+    indicator.execute("colorWhenOnOrOff", new groovy.json.JsonBuilder([color: "blue"]).toString())
+}
