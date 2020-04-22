1bc2bf110c8f200bb506a7286fc9d8d5846d39c1
Ovidiu Pruteanu <ovidiu@wmtek.com>
2020-04-22 11:53:35 +0300

    Remove "Refresh" capability from Fibaro Smart Implant Contact DTH
    It was causing the sensor to report as closed whenever the refresh command was issued.

--- devicetypes/ovidiupruteanu/fibaro-smart-implant-contact.src/fibaro-smart-implant-contact.groovy
+++ devicetypes/ovidiupruteanu/fibaro-smart-implant-contact.src/fibaro-smart-implant-contact.groovy
@@ -18,7 +18,6 @@ metadata {
     definition(name: "Fibaro Smart Implant Contact", namespace: "ovidiupruteanu", author: "Ovidiu Pruteanu") {
         capability "Contact Sensor"
         capability "Health Check"
-        capability "Refresh"
         capability "Sensor"
     }
 
@@ -31,12 +30,8 @@ metadata {
             state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#e86d13")
         }
 
-        standardTile("refresh", "device.contact", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-            state "default", label: '', action: "refresh.refresh", icon: "st.secondary.refresh"
-        }
-
         main "contact"
-        details(["contact", "refresh"])
+        details(["contact"])
     }
 }
 
@@ -50,9 +45,6 @@ def updated() {
 }
 
 def configure() {
-    // Device-Watch simply pings if no device events received for checkInterval duration 1 hour
-    sendEvent(name: "checkInterval", value: 1 * 60 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: parent.hubID, offlinePingable: "1"])
-    refresh()
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.notificationv3.NotificationReport cmd) {
@@ -66,15 +58,4 @@ def zwaveEvent(physicalgraph.zwave.commands.notificationv3.NotificationReport cm
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
     // Handles all Z-Wave commands we aren't interested in
     [:]
-}
-
-/**
- * PING is used by Device-Watch in attempt to reach the Device
- * */
-def ping() {
-    refresh()
-}
-
-def refresh() {
-    parent.contactRefresh(device.deviceNetworkId)
 }
